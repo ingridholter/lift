ab060e9481c70ed2aedd12922d14f8c7500dcdb6
Ingrid Holter Norland <ingrnor@gmail.com>
2020-02-19 15:36:36 +0100

    added drive.c

---
+++ drive.c
@@ -0,0 +1,39 @@
+printf("velg retning")
+
+int array[] {0};
+
+
+int prevFloor = -1;
+for (i=0; i<4;i++){
+    if(hardware_floor_sensor(i)==1)
+        int prevFloor = currentFloor;
+        int currentFloor = hardware_floor_sensor(i);
+        return currentFloor;
+} //skal vi gjøre med prevFloor
+
+int setDirection()
+{
+    int currentFloor; //finnes
+    int direction;
+    for (i=0; i<10, i++)
+    {
+        array[i][1]
+        if(array[i][1]==inside) //bestilt fra insiden
+        {
+            if(array[i][0]>currentFloor){
+                direction = up;
+                return direction;
+            }
+            if (array[i][0]<currentFloor){
+                direction = down;
+                return direction;
+            }
+        }
+        //hva mer skal gjøres her;
+    }
+    
+}
+
+
+
+//må ha en neste tilstand varibabel
