13e990f15314718e1f7432b0412944cb61548c5f
Ingrid Holter Norland <ingrnor@gmail.com>
2020-02-21 11:54:12 +0100

    added functions

--- liftOrder.h
+++ liftOrder.h
@@ -5,3 +5,26 @@
 
 //functions
 
+void setLiftOrders();
+
+void removeOrders(int CurrentFloor);
+
+void removeAllOrders();
+
+void getLiftOrders(); //returnere array?
+
+
+//returnerer 1 hvis den skal stoppe i etasje og 0 hvis ikke
+int isCurrentFloorDemanded(int CurrentFloor);
+
+//setter heisens retning 1:ned og 0:opp
+//går gjennom array og sjekker
+int setDirection();
+
+//er heisen krevd over gjeldene etasje
+int ordered_above(int CurrentFloor);
+
+//er heisen krev under gjeldende etasje
+int ordered_below(int CurrentFloor);
+
+
