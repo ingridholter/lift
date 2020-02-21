a49282177a023087548ba9f15b5f2649f583e443
birgolini <59916516+birgolini@users.noreply.github.com>
2020-02-21 13:09:44 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,2 +1,26 @@
-printf("hei")
+#include "hardware.h"
+#include "drive.h"
 
+//Initialiserer
+bool driveToDefinedState() {
+    int hardware_init ();
+    // If “ikke i etasje” {kjør ned til etasje}
+    //  if “etasje” {sett etasje+lys, åpne for bestillinger}
+    return TRUE;
+}
+
+void main {
+    
+    bool initialized = driveToDefinedState();
+    
+    while (initialized) {
+        //Poller hardware
+        int stopSignal = hardware_read_stop_signal();
+        int obstruction = hardware_read_obstruction_signal();
+        
+        checkOrderButtons();
+        
+        stateMachine();
+        
+    }
+}
