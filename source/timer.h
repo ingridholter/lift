47134a55b8ba7f979719ff0a4da379a813f93ee9
ingridholter <59916598+ingridholter@users.noreply.github.com>
2020-02-23 10:33:22 +0100

    added doxygen comments

--- source/timer.h
+++ source/timer.h
@@ -1,12 +1,24 @@
+/**
+* @file
+* @brief The Timer module. A library containing functions regarding the timer
+*/
+
 #ifndef _TIMER__
 #define _TIMER__
 
 #include <time.h>
 
-//resets timer
+/**
+* @brief Sets a new timestamp
+*/
 void timer_reset();
 
 //Checks if more than TIME_LIMIT seconds since last ?timestamp?
+
+/**
+* @brief Checks if more than TIME_LIMIT seconds have passed since last time timer was reset
+* @return 1 if the time limit has passed, 0 otherwise.
+*/
 int timer_expired();
 
 
