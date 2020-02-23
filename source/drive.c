58dfa9dc82bdfd1232bc95cc226372bfea36f9dc
birgolini <59916516+birgolini@users.noreply.github.com>
2020-02-23 10:19:58 +0100

    Update drive.c

--- source/drive.c
+++ source/drive.c
@@ -11,6 +11,30 @@ for (i=0; i<4;i++){
         return currentFloor;
 } //skal vi gjøre med prevFloor
 
+//Initializing
+bool driveToDefinedState() {
+    int error = hardware_init();
+    if (error != 0) {
+        printf("Unable to initialize hardware\n");
+        return FALSE;
+    }
+    
+    while (currentFloor > 3) {
+    //Checks if lift is at floor
+        for (floor = 0; i < 4; i++) {
+            if (hardware_read_floor_sensor(floor) == 1)
+                currentFloor = floor;
+        }
+        hardware_command_movement (HARDWARE_MOVEMENT_DOWN)
+    }
+    
+    hardware_command_movement (HARDWARE_MOVEMENT_STOP)
+    //Changes floor light to given floor
+    void hardware_command_floor_indicator_on (currentFloor)
+    
+    return TRUE;
+}
+
 int setDirection()
 {
     int currentFloor; //finnes
