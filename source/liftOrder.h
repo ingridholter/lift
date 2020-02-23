a11b05a49bcd1d487d32f89da6a1f3f4f7850c0f
Birgit S Øygard <birgitoy@stud.ntnu.no>
2020-02-23 12:30:43 +0100

    Update liftOrder.h

--- source/liftOrder.h
+++ source/liftOrder.h
@@ -32,7 +32,7 @@ void removeAllOrders();
 * @brief Stops the lift when it arrives at a floor where it is demanded
 *in the same direction that it is driving as well as at the floors where someone wants to get out of the elevator.
 */
-int isCurrentFloorDemanded(int currentFloor, int currentDir);
+int isCurrentFloorDemanded(int currentFloor, HardwareMovement currentDir);
 
 //setter heisens retning 1:ned og 0:opp
 //går gjennom array og sjekker
