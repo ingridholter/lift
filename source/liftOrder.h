2e98d6ac2dbd7c1f08b5abaa0e00d5f2311639cd
ingridholter <59916598+ingridholter@users.noreply.github.com>
2020-02-23 10:40:41 +0100

    Update liftOrder.h

--- source/liftOrder.h
+++ source/liftOrder.h
@@ -1,3 +1,9 @@
+/**
+* @file
+* @brief A library containing the LiftOrder module
+*/
+
+
 #ifndef LIFTORDER_H__
 #define LIFTORDER_H__
 
@@ -5,26 +11,45 @@
 
 //functions
 
+/**
+* @brief
+*/
 void setLiftOrders();
 
+/**
+* @brief
+*/
 void removeOrders(int CurrentFloor);
 
+/**
+* @brief
+*/
 void removeAllOrders();
-
-void getLiftOrders(); //returnere array?
 
 
 //returnerer 1 hvis den skal stoppe i etasje og 0 hvis ikke
+/**
+* @brief
+*/
 int isCurrentFloorDemanded(int CurrentFloor);
 
 //setter heisens retning 1:ned og 0:opp
 //går gjennom array og sjekker
+/**
+* @brief
+*/
 int setDirection();
 
 //er heisen krevd over gjeldene etasje
+/**
+* @brief
+*/
 int ordered_above(int CurrentFloor);
 
 //er heisen krev under gjeldende etasje
+/**
+* @brief
+*/
 int ordered_below(int CurrentFloor);
 
 
