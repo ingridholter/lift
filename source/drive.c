2e490577871e2664fd26efd105071476310be75c
Birgit S Øygard <birgitoy@stud.ntnu.no>
2020-02-24 12:23:19 +0100

    Update drive.c

--- source/drive.c
+++ source/drive.c
@@ -21,7 +21,6 @@ int driveToDefinedState() {
     hardware_command_movement(HARDWARE_MOVEMENT_STOP);
     //Changes floor light to given floor
     hardware_command_floor_indicator_on(currFloor);
-    currentDir = HARDWARE_MOVEMENT_DOWN;
     currentState = levelClosed;
     return 1;
 }
