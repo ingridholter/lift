bd8ad1aa2b9b3e1129c77ff9eda793297692297e
Birgit S Øygard <birgitoy@stud.ntnu.no>
2020-02-25 12:14:02 +0100

    Update main.c

--- source/main.c
+++ source/main.c
@@ -13,20 +13,10 @@
 */
 
 int main() {
-    
     int initialized = driveToDefinedState();
     
     while (initialized) {
-        /*
-        //Poller hardware
-        int stopSignal = hardware_read_stop_signal();
-        int obstruction = hardware_read_obstruction_signal();
-        
-        setLiftOrders(); // Checks order buttons
-        */
-        
         stateMachine();
-        
     }
     return 0;
 }
