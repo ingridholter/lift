b9118d151dee9a27ca6773f558241d59abc15b28
ingridholter <59916598+ingridholter@users.noreply.github.com>
2020-02-25 15:49:11 +0100

    Update drive.c

--- source/drive.c
+++ source/drive.c
@@ -84,7 +84,7 @@ void stateMachine() {
             if (isCurrentFloorDemanded(currentFloor, currentDir)&& atFloor()>=0) {
                 hardware_command_movement(HARDWARE_MOVEMENT_STOP);
                 hardware_command_door_open(1);
-                timerReset();
+                //timerReset();
                 currentState = levelOpen;
             }
             break;
