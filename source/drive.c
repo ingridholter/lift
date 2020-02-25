cda6c0bb2bc0d7e2c42c39527680ae96086e30a7
Birgit S Øygard <birgitoy@stud.ntnu.no>
2020-02-25 12:51:54 +0100

    Update drive.c

--- source/drive.c
+++ source/drive.c
@@ -13,7 +13,7 @@ int driveToDefinedState() {
         hardware_command_movement(HARDWARE_MOVEMENT_DOWN);
     }
     hardware_command_movement(HARDWARE_MOVEMENT_STOP);
-    currentDir = HARDWARE_MOVEMENT_DOWN;
+    //currentDir = HARDWARE_MOVEMENT_DOWN;
     currentState = levelClosed;
     return 1;
 }
