81c7aac3386eb364faf3144c92217fbcdcb93065
Birgit S Øygard <birgitoy@stud.ntnu.no>
2020-02-26 12:50:44 +0100

    Update drive.c

--- source/drive.c
+++ source/drive.c
@@ -21,11 +21,9 @@ void stateMachine() {
     stopSignal = hardware_read_stop_signal();
     hardware_command_stop_light(stopSignal);
     if (stopSignal) {
-        //hardware_command_stop_light(1);
         removeAllOrders();
     }
     else {
-        //hardware_command_stop_light(0);
         setLiftOrders(); //Checks order buttons
     }
     
