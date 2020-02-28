ae918bbe9f5d655ec7270d6cbbde78ec399852b2
Birgit S Øygard <birgitoy@stud.ntnu.no>
2020-02-28 12:10:47 +0100

    Update timer.h

--- source/timer.h
+++ source/timer.h
@@ -1,6 +1,6 @@
 /**
  * @file
- * @brief The Timer module. A library containing functions regarding the timer
+ * @brief The timer module. A library containing functions regarding the timer.
  */
 
 #ifndef _TIMER__
@@ -9,12 +9,12 @@
 #include <time.h>
 
 /**
- * @brief Sets a new timestamp
+ * @brief Sets a new timestamp.
  */
 void timerReset();
 
 /**
- * @brief Checks if more than timeLimit seconds have passed since last time timer was reset
+ * @brief Checks if more than 3 seconds have passed since timerReset().
  * @return 1 if the time limit has passed, 0 otherwise.
   */
 int timerExpired();
