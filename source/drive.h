20c274ed5f10d2d323f06aebca814c38c23466c1
Birgit S Øygard <birgitoy@stud.ntnu.no>
2020-02-28 14:30:29 +0100

    Update drive.h

--- source/drive.h
+++ source/drive.h
@@ -1,7 +1,6 @@
 /**
  * @file
- * @brief The drive module.  The stateMachine() sets the lift's direction and motorpower in order to get desired behavior.
- *
+ * @brief Drives the lift first by initializing and then by using different states.
  */
 #ifndef _DRIVE__
 #define _DRIVE__
@@ -20,44 +19,14 @@ typedef enum{
 }liftState;
 
 /**
- * @brief A 0-3 integer value for the last floor the lift was at.
- */
-int currentFloor;
-
-/**
- * @brief A 0-3 integer value for the next floor the lift will reach.
- */
-int nextFloor;
-
-/**
- * @brief A 1-3 integer value for the areas between the floors.
- */
-int betweenFloor;
-
-/**
- * @brief A 0 or 1 integer value. 1 if @c hardware_read_stop_signal is high, 0 if not.
- */
-int stopSignal;
-
-/**
- * @brief A HardwareMovement type for the lift's current direction.
- */
-HardwareMovement currentDir;
-
-/**
- * @brief Holds the current state of the lift
- */
-liftState currentState;
-
-/**
  * @brief Initializes lift.
- * @return 0 if not able to initialize. 1 if inizialized.
+ *
+ * @return 1 on success; 0 otherwise.
  */
 int driveToDefinedState();
 
 /**
- * @brief State Machine, decides the lift state. sets the lift's direction and motorpower in order to get desired behavior.
- *
+ * @brief Sets the @c currentState. Decides the lift's direction and motorpower in order to get desired behavior.
  */
 void stateMachine();
 
