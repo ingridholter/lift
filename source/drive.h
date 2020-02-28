bc8bed054abb1c2bdca21930b20da34b56050d07
Birgit S Øygard <birgitoy@stud.ntnu.no>
2020-02-28 12:36:00 +0100

    Update drive.h

--- source/drive.h
+++ source/drive.h
@@ -1,6 +1,6 @@
 /**
  * @file
- * @brief The drive module. The stateMachine() sets the lift's direction and motorpower in order to get desired behavior.
+ * @brief The drive module.  The stateMachine() sets the lift's direction and motorpower in order to get desired behavior.
  *
  */
 #ifndef _DRIVE__
@@ -10,8 +10,7 @@
 #include "timer.h"
 
 /**
- * @brief Order type used in @c hardware_read_order
- * and in @c hardware_command_order_light.
+ * @brief Lift state used to switch between states in @c stateMachine.
  */
 typedef enum{
   levelOpen,
@@ -21,27 +20,27 @@ typedef enum{
 }liftState;
 
 /**
- * @brief A 0-3 integer value for the current floor the lift is at. 
+ * @brief A 0-3 integer value for the last floor the lift was at.
  */
 int currentFloor;
 
 /**
- * @brief A 0-3 integer value for the next floor the lift is at.
+ * @brief A 0-3 integer value for the next floor the lift will reach.
  */
 int nextFloor;
 
 /**
- * @brief A 1-3 integer value for the areas between floors. 
+ * @brief A 1-3 integer value for the areas between the floors.
  */
 int betweenFloor;
 
 /**
- * @brief A 0 or 1 integer value. 1 if stopSignal is high, 0 if not.
+ * @brief A 0 or 1 integer value. 1 if @c hardware_read_stop_signal is high, 0 if not.
  */
 int stopSignal;
 
 /**
- * @brief A HardwareMovement type. The current direction of the lift. 
+ * @brief A HardwareMovement type for the lift's current direction.
  */
 HardwareMovement currentDir;
 
