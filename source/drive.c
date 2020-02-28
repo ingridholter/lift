9114df74ff185c01de6858215d1adca49a00d224
Birgit S Øygard <birgitoy@stud.ntnu.no>
2020-02-28 08:16:22 +0100

    Update drive.c

--- source/drive.c
+++ source/drive.c
@@ -50,7 +50,7 @@ void stateMachine() {
             if (getFloorNumber() >= 0) {
                 currentFloor = getFloorNumber();
             }
-            else {
+            else if (prevState != stationaryBetweenFloors) {
                 betweenFloor = updateBetweenFloor(currentDir, currentFloor);
             }
             if (stopSignal) {
