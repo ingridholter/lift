cf1da20d203c3b30566a89ec37d10dba5cccfb39
Birgit S Øygard <birgitoy@stud.ntnu.no>
2020-02-28 09:52:15 +0100

    Update drive.h

--- source/drive.h
+++ source/drive.h
@@ -1,8 +1,8 @@
 /**
-* @file
-* @brief Sets the lift's direction and motorpower in order to get desired behavior.
-*
-*/
+ * @file
+ * @brief Sets the lift's direction and motorpower in order to get desired behavior.
+ *
+ */
 #ifndef _DRIVE__
 #define _DRIVE__
 
@@ -10,9 +10,9 @@
 #include "timer.h"
 
 /**
-* @brief Order type used in @c hardware_read_order
-* and in @c hardware_command_order_light.
-*/
+ * @brief Order type used in @c hardware_read_order
+ * and in @c hardware_command_order_light.
+ */
 typedef enum{
   levelOpen,
   levelClosed,
@@ -22,38 +22,38 @@ typedef enum{
 
 /**
  * @brief A 0-3 integer value for the current floor the lift is at. 
-  */
+ */
 int currentFloor;
 
 /**
  * @brief A 0-3 integer value for the next floor the lift is at.
-  */
+ */
 int nextFloor;
 
 /**
  * @brief A 1-3 integer value for the areas between floors. 
-  */
+ */
 int betweenFloor;
 
 /**
  * @brief A 0 or 1 integer value. 1 if stopSignal is high, 0 if not.
-  */
+ */
 int stopSignal;
 
 /**
  * @brief A HardwareMovement type. The current direction of the lift. 
-  */
+ */
 HardwareMovement currentDir;
 
 /**
  * @brief Holds the current state of the lift
-  */
+ */
 liftState currentState;
 
 /**
-* @brief Initializes lift.
-* @return 0 if not able to initialize. 1 if inizialized. 
-*/
+ * @brief Initializes lift.
+ * @return 0 if not able to initialize. 1 if inizialized.
+ */
 int driveToDefinedState();
 
 /**
