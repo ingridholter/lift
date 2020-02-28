7c1a8ea072da85a917557923ac69c89eb9c0611f
Birgit S Øygard <birgitoy@stud.ntnu.no>
2020-02-28 14:30:34 +0100

    Update liftOrder.h

--- source/liftOrder.h
+++ source/liftOrder.h
@@ -1,6 +1,6 @@
 /**
  * @file
- * @brief A library containing the LiftOrder module
+ * @brief Handles all orders of the lift.
  */
 
 #ifndef LIFTORDER_H__
@@ -10,99 +10,132 @@
 #include <stdio.h>
 
 /**
- * @brief Gives the correct value of the down orders in the liftOrders array. 
- * @param[in] floor 0-3 integer value of where the lift has been ordered.
+ * @brief Checks down orders.
+ *
+ * @param floor The floor that is being checked for down orders.
+ *
+ * @return Index of the down order at @p floor in @c liftOrders[].
  */
 int ordersDown(int floor);
 
 /**
- * @brief Gives the correct value of the inside orders in the liftOrders array.
- * @param[in] floor 0-3 integer value of where the lift has been ordered.
+ * @brief Checks inside orders.
+ *
+ * @param floor The floor that is being checked for inside orders.
+ *
+ * @return Index of the inside order at @p floor in @c liftOrders[].
  */
 int ordersInside(int floor);
 
 /**
- * @brief Gives the correct value of the up orders in the liftOrders array.
- * @param[in] floor 0-3 integer value of where the lift has been ordered.
+ * @brief Checks up orders.
+ *
+ * @param floor The floor that is being checked for up orders.
+ *
+ * @return Index of the up order at @p floor in @c liftOrders[].
  */
 int ordersUp(int floor);
 
 /**
- * @brief Checks if the lift is ordered and sets the liftOrders array. Lights the order lights accordingly
+ * @brief Checks all the order buttons.
+ * Sets @c liftOrders[] and order lights accordingly.
  */
 void setOrders();
 
 /**
- * @brief removes handled orders from liftOrders array. Turnes of lights accordingly
- * @param[in] currFloor The floor the lift is currently on. Must be an integer in range 0-3.
+ * @brief Removes orders at @p currFloor from @c liftOrders[] and turnes off the order lights.
+ *
+ * @param currFloor The current floor where orders have been handled.
  */
 void clearOrders(int currFloor);
 
 /**
- * @brief removes all orders in liftOrder array. Turnes off all order lights
+ * @brief Removes all the orders in @c liftOrders[] and turnes off all the order lights.
  */
 void clearAllOrders();
 
 /**
- * @brief  Checks if lift is ordered above currFloor
- * @param[in] currFloor The floor the lift is currently on. Must be an integer in range 0-3.
+ * @brief Checks if the lift is ordered above @p currFloor.
+ *
+ * @param currFloor The current floor.
+ *
+ * @return 1 if there is an order above @p currFloor; 0 otherwise.
  */
 int orderedAbove(int currFloor);
 
 /**
- * @brief Checks if lift is ordered below currFloor.
- * @param[in] currFloor The floor the lift is currently on. Must be an integer in range 0-3.
+ * @brief Checks if the lift is ordered below @p currFloor.
+ *
+ * @param currFloor The current floor.
+ *
+ * @return 1 if there is an order below @p currFloor; 0 otherwise.
  */
 int orderedBelow(int currFloor);
 
 /**
- * @brief Checks if there is any orders in the liftOrders array.
+ * @brief Checks for orders in @c liftOrders[].
+ *
+ * @return 1 if there are any orders; 0 otherwise.
  */
 int checkIfOrders();
 
 /**
- * @brief Decides where the lift should go next and sets the motor direction.
- * @param[in] currFloor The floor the lift is currently on. Must be an integer in range 0-3.
- * @param[in] currDir The direction of the lift. Must be of enum type HardwareMovement.
- * @param[in] betwFloor A value for the areas between floors. Must be an integer in range 1-3.
- * @return HardwareMovement enum type, the new direction for the lift.
+ * @brief Decides which direction the lift should go next.
+ * If there are orders above and below @p currFloor, the lift continues in @p currDir.
+ * This is unless @p currDir = HARDWARE_MOVEMENT_STOP; the lift will then choose to go down.
+ *
+ * @param currFloor The current floor.
+ * @param currDir The current direction.
+ * @param betwFloor The floor directly above; 0 if the lift is at a floor.
+ *
+ * @return The new direction.
  */
 HardwareMovement setDirection(int currFloor, HardwareMovement currDir, int betwFloor);
 
 /**
- * @brief Stops the lift when it arrives at a floor where it is demanded in the same direction that it is driving as well as at the floors where someone wants to get out of the lift.
- * @param[in] currFloor The floor the lift is currently on. Must be an integer in range 0-3.
- * @param[in] currDir The direction of the lift. Must be of enum type HardwareMovement
- * @return 1 if the lift should stop at currFloor, 0 otherwise
+ * @brief Decides if the lift should stop.
+ * The lift should stop if it has an inside order at @p currFloor, or if it is ordered in @p currDir.
+ *
+ * @param currFloor The current floor.
+ * @param currDir The current direction.
+ *
+ * @return 1 if the lift should stop at @p currFloor; 0 otherwise.
  */
 int isCurrentFloorDemanded(int currFloor, HardwareMovement currDir);
 
 /**
- * @brief Checks if lift is ordered at floor.
- * @param[in] currFloor The floor the lift is currently on. Must be an integer in range 0-3.
- * @return 1 if the lift is ordered at floor, 0 if not.
+ * @brief Checks if lift is ordered at @p currFloor.
+ *
+ * @param currFloor The current floor.
+ *
+ * @return 1 if the lift is ordered at @p currFloor; 0 otherwise.
  */
 int isFloorOrdered(int currFloor);
 
 /**
- * @brief Gives the value of the lifts floor.
- * @return 0-3 integer of the floor the lift is currently at.
+ * @brief Checks which floor the lift is currently on.
+ *
+ * @return The current floor; -1 if the lift is not at a floor.
  */
 int getFloorNumber();
 
 /**
- * @brief Updates the nextFloor variable.
- * @param[in] currDir The direction of the lift. Must be of enum type HardwareMovement
- * @param[in] currFloor lalala
- * @return 1-3 integer value. Gives the next floor the lift will reach, given the current direction.
+ * @brief Updates @c nextFloor.
+ *
+ * @param currDir The current direction.
+ * @param currFloor The current floor.
+ *
+ * @return The next floor the lift will reach if @p currDir is not changed.
  */
 int updateNextFloor(int currFloor, HardwareMovement currDir);
 
 /**
- * @brief Updates the betweenfloor variable.
- * @param[in] nFloor The direction of the lift. Must be of enum type HardwareMovement
- * @param[in] currFloor The floor the lift is currently on. Must be an integer in range 0-3.
- * @return 1-3 integer value. Gives which floor the lift is directly under.
+ * @brief Updates the @c betweenFloor.
+ *
+ * @param nFloor The next floor the lift will reach.
+ * @param currFloor The current floor.
+ *
+ * @return The floor the lift is directly under.
  */
 int updateBetweenFloor(int nFloor, int currFloor);
 
