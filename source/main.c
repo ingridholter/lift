58ebfcdc541152207f2f80ee1f1fde7f09668276
Birgit S Øygard <birgitoy@stud.ntnu.no>
2020-02-28 12:35:55 +0100

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1,13 +1,5 @@
-/**
- * @file
- * @brief The main function of the application
- */
-
 #include "drive.h"
 
-/**
- * @brief main
- */
 int main() {
     int initialized = driveToDefinedState();
     
