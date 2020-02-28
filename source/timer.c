64a2c6569c26a71fffad511f9e545b3dc0024753
Birgit S Øygard <birgitoy@stud.ntnu.no>
2020-02-28 10:30:41 +0100

    Update timer.c

--- source/timer.c
+++ source/timer.c
@@ -1,18 +1,4 @@
-/**
-* @file
-* @brief Implementation of the timer module
-*/
-
 #include "timer.h"
-/**
-* @brief Checks if the lift is ordered and sets the liftOrders array. Lights the order lights accordingly
-*/
-const int timeLimit = 3;
-
-/**
-* @brief Checks if the lift is ordered and sets the liftOrders array. Lights the order lights accordingly
-*/
-static clock_t clockVar;
 
 void timerReset(){
 	clockVar = clock();
